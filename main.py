b4c0a662def81717b784b12b1af2088ce9c79bb9
signebedi <signe@siftingwinnowing.com>
2020-07-12 05:26:35 +0000

    updated requirements and main

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import json, requests, re
 import pandas as pd
 
 # get a dictionary as a json object
-dictionary = json.loads(requests.get('https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json').text)
+# dictionary = json.loads(requests.get('https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json').text)
 # print(dictionary)
 
 
-tlds = pd.read_html('https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains')
+tlds = requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt')
+for line in tlds:
+    print(line)
 
-git config --global credential.helper store
+
+# tlds = pd.read_html('https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains')
