a90d0dbe04ac44a5b01d9e91b6dadef30b368b6d
signebedi <signe@siftingwinnowing.com>
2020-07-12 06:11:36 +0000

    made main object oriented

--- main.py
+++ main.py
@@ -1,49 +1,38 @@
 import json, requests, re
 import pandas as pd
 
-# get a dictionary as a json object
-dictionary = json.loads(requests.get('https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json').text)
-# print(dictionary)
+def get_matches():
+    # get a dictionary as a json object
+    dictionary = json.loads(requests.get('https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json').text)
 
-# dictionary = { # temporary dictionary to start testing the functionality
-#     'a':'def',
-#     'com':'def',
-#     'co':'def',
-#     'aharry':'def',
-#     'temporio':'def',
-#     'shipsh':'def',
-#     'tencom':'def',
-#     'builde':'def',
-#     'misc':'def',
-#     'borg':'def',
-#     'apple':'def',
-#     'thas':'def',
-# }
-
-# tlds = pd.read_html('https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains')
-r = requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt', stream=True)
-
-tlds = []
-
-for line in r.iter_lines():
-    if line: 
-        tlds.append(line.decode().lower())
-    # print(tlds)
+    # create empty list to store top level domains
+    tlds = []
     
-# for word in dictionary.keys():
-#     if word in tlds:
-#         print(word)
+    # pull data from IANA
+    r = requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt', stream=True)
 
-# pat = "$|".join(tlds)
+    # iterate through text file and add to list
+    for line in r.iter_lines():
+        if line: 
+            tlds.append(line.decode().lower())
 
-# for word in dictionary.keys():
-#     print(word, ': ', re.findall(pat, word))
-matches = []
-for word in dictionary.keys():
-    for tld in tlds:
-        if word.lower().endswith(tld):
-            matches.append({word.lower():tld})
-# print(matches)
+    # create another empty list to store matches
+    matches = []
+    for word in dictionary.keys():
+        for tld in tlds:
+            if word.lower().endswith(tld):
+                matches.append({word.lower():tld})
 
-with open('output.txt', 'w') as f:
-    f.write(str(matches))
+    return matches
+    
+def main():
+
+    matches = get_matches()
+    
+    with open('output.txt', 'w') as f:
+        f.write(str(matches))
+        
+        
+        
+if __name__ == '__main__':
+    main()
