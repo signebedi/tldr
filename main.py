2517da6b153fc79a69c0b8ad17c96bf6c54c5a67
signebedi <signe@siftingwinnowing.com>
2020-07-12 05:58:09 +0000

    got things working (rather inefficiently...)

--- main.py
+++ main.py
@@ -2,35 +2,48 @@ import json, requests, re
 import pandas as pd
 
 # get a dictionary as a json object
-# dictionary = json.loads(requests.get('https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json').text)
+dictionary = json.loads(requests.get('https://raw.githubusercontent.com/adambom/dictionary/master/dictionary.json').text)
 # print(dictionary)
 
-dictionary = { # temporary dictionary to start testing the functionality
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
-    'a':'def',
+# dictionary = { # temporary dictionary to start testing the functionality
+#     'a':'def',
+#     'com':'def',
+#     'co':'def',
+#     'aharry':'def',
+#     'temporio':'def',
+#     'shipsh':'def',
+#     'tencom':'def',
+#     'builde':'def',
+#     'misc':'def',
+#     'borg':'def',
+#     'apple':'def',
+#     'thas':'def',
+# }
 
-}
-
-
+# tlds = pd.read_html('https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains')
 r = requests.get('https://data.iana.org/TLD/tlds-alpha-by-domain.txt', stream=True)
 
 tlds = []
 
 for line in r.iter_lines():
     if line: 
-        tlds.append(line.decode())
+        tlds.append(line.decode().lower())
     # print(tlds)
     
-    
-    
-# tlds = pd.read_html('https://en.wikipedia.org/wiki/List_of_Internet_top-level_domains')
+# for word in dictionary.keys():
+#     if word in tlds:
+#         print(word)
+
+# pat = "$|".join(tlds)
+
+# for word in dictionary.keys():
+#     print(word, ': ', re.findall(pat, word))
+matches = []
+for word in dictionary.keys():
+    for tld in tlds:
+        if word.lower().endswith(tld):
+            matches.append({word:tld})
+print(matches)
+
+with open('output.txt', 'w') as f:
+    f.write(str(matches))
